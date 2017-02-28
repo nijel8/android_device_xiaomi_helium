a687d14622992ab4410de3ae09afba0c19c776d6
nijel8 <nijel8@gmail.com>
2017-02-28 18:18:45 -0500

    libril: bring inline with ril-caf, fixes development settings

--- libril/Android.mk
+++ libril/Android.mk
@@ -22,8 +22,6 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_STATIC_LIBRARIES := \
     libprotobuf-c-nano-enable_malloc \
 
-#LOCAL_CFLAGS := -DANDROID_MULTI_SIM -DDSDA_RILD1
-
 ifeq ($(SIM_COUNT), 2)
     LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
 endif
@@ -31,14 +29,32 @@ endif
 LOCAL_C_INCLUDES += external/nanopb-c
 LOCAL_C_INCLUDES += hardware/ril-caf/include
 LOCAL_EXPORT_C_INCLUDE_DIRS := hardware/ril-caf/include
-#LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/librilutils
 
 LOCAL_MODULE:= libril
-LOCAL_CLANG := false
-
-#LOCAL_COPY_HEADERS_TO := libril
-#LOCAL_COPY_HEADERS := ril_ex.h
+LOCAL_CLANG := true
+LOCAL_SANITIZE := integer
 
 include $(BUILD_SHARED_LIBRARY)
 
+
+# For RdoServD which needs a static library
+# =========================================
+ifneq ($(ANDROID_BIONIC_TRANSITION),)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    ril.cpp
+
+LOCAL_STATIC_LIBRARIES := \
+    libutils_static \
+    libcutils \
+    librilutils_static \
+    libprotobuf-c-nano-enable_malloc
+
+LOCAL_CFLAGS :=
+
+LOCAL_MODULE:= libril_static
+
+include $(BUILD_STATIC_LIBRARY)
+endif # ANDROID_BIONIC_TRANSITION
 endif # BOARD_PROVIDES_LIBRIL
