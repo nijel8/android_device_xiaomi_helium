95bfdfd4e947e0b112f45513d0f75278bcf20669
TheStrix <parthbhatia@lineageos.org>
2017-03-20 20:49:52 +0530

    hydrogen: Update build fingerprint
     * From China Stable V8.2.3.0.MBCCNDL (MIUI8)
    
    Change-Id: I8c1841bf3ab5b9de5586004361f19c71266c2dc6

--- lineage.mk
+++ lineage.mk
@@ -37,6 +37,6 @@ WITH_SU := true
 # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/helium/helium:7.0/NRD90M/7.3.16:user/release-keys \
-    PRIVATE_BUILD_DESC="helium-user 7.0 NRD90M 7.3.16 release-keys"
+    BUILD_FINGERPRINT=Xiaomi/hydrogen/hydrogen:6.0.1/MMB29M/V8.2.3.0.MBCCNDL:user/release-keys \
+    PRIVATE_BUILD_DESC="hydrogen-user 6.0.1 MMB29M V8.2.3.0.MBCCNDL release-keys"
 endif
