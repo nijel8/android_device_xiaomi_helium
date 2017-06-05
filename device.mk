02b248f98227913f709791684c8d9fe02b973aaf
nijel8 <nijel8@gmail.com>
2017-06-05 09:55:33 -0400

    core_ctl: rework the load/unload routine [2/2]

--- device.mk
+++ device.mk
@@ -23,8 +23,11 @@ $(call inherit-product-if-exists, vendor/xiaomi/helium/helium-vendor.mk)
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    init.qcom.power.rc \
-    init.core.ctl.sh
+    init.qcom.power.rc
+
+# CPU congiguration    
+PRODUCT_PACKAGES += \
+    cpu.conf
 
 # Inherit from msm8956-common
 $(call inherit-product, device/xiaomi/msm8956-common/msm8956.mk)
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,8 +9,10 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.core.ctl.sh
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE		:= cpu.conf
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.core.ctl.sh
+LOCAL_SRC_FILES		:= etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_ETC)/cpu
+LOCAL_POST_INSTALL_CMD  := chmod 755 $(TARGET_OUT_ETC)/cpu/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
